4126a1d3765a0a71df9d3cbe844ad14c9d7af914
beom-beom <128284680+beom-beom@users.noreply.github.com>
2023-09-16 17:53:58 +0900

    http_webserver.c
    #gif,mp3,img

--- http_webserver/http_webserver.c
+++ http_webserver/http_webserver.c
@@ -1,145 +1,188 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-#include <stdbool.h>
 
-#define BUFF_SIZE 1024
-#define SMALL_BUFF 100
-#define SERV_SIZE 32
-void *request_handler(void*arg);
-void send_data(FILE*fp,char * ct,char* file_name);
-char* content_type(char* file);
-void send_error(FILE *fp);
+#define BUF_SIZE 1000
+#define HEADER_FMT "HTTP/1.1 %d %s\nContent-Length: %ld\nContent-Type: %s\n\n"
+#define NOT_FOUND_CONTENT       "<h1>404 Not Found</h1>\n"
+#define SERVER_ERROR_CONTENT    "<h1>500 Internal Server Error</h1>\n"
+
+void fill_header(char *header, int status, long len, char *type);
 void error_handling(char *message);
-void bind_and_listen(int serv_sock, struct sockaddr_in* serv_adr, int backlog, int port);
-void accept_connection(int serv_sock, struct sockaddr_in clnt_addr,int clnt_sock,socklen_t clnt_addr_size);
-int error_message(int argc,char *argv[]);
-int main(int argc, char *argv[]){
-	int serv_sock, clnt_sock;
-	struct sockaddr_in serv_addr, clnt_addr;
-	socklen_t clnt_addr_size;
-	error_message(argc,argv);
-	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-	bind_and_listen(serv_sock, &serv_addr, 20, atoi(argv[1]));
-	accept_connection(serv_sock, clnt_addr,clnt_sock,clnt_addr_size);
-	return 0;
+void handle_404(int asock);
+void handle_500(int asock);
+void *client_handler(void *arg);
+void accept_connection(struct sockaddr_in clnt_addr,int clnt_sock,socklen_t clnt_addr_size);
+void bind_and_listen(int serv_sock, int backlog, int port);
+
+int main(int argc, char *argv[]) {
+    int port;
+   int serv_sock;
+   struct sockaddr_in clnt_addr;
+   socklen_t clnt_addr_size;
+
+
+   if(argc!=2){
+           printf("Usage : %s <port>\n", argv[0]);
+           return 0;
+      }
+
+   serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+   bind_and_listen(serv_sock, 20, atoi(argv[1]));
+   accept_connection(clnt_addr,serv_sock,clnt_addr_size);
+   return 0;
 }
-int error_message(int argc,char *argv[]){
-	if(argc!=2){
-                 printf("Usage : %s <port>\n", argv[0]);
-                 return 0;
-         }
-}
-void bind_and_listen(int serv_sock, struct sockaddr_in* serv_adr, int backlog, int port){	
-	memset(serv_adr, 0, sizeof(struct sockaddr_in));
-	serv_adr->sin_family = AF_INET;
-	serv_adr->sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_adr->sin_port = htons(port);
-   	if(bind(serv_sock, (struct sockaddr*)serv_adr, sizeof(struct sockaddr_in)) == -1)
+void bind_and_listen(int serv_sock, int backlog, int port){	
+	 struct sockaddr_in sin;
+
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_port = htons(port);
+ 	if(bind(serv_sock, (struct sockaddr *)&sin, sizeof(sin)) == -1)
     		error_handling("bind() error");
     	if(listen(serv_sock, backlog) == -1)
     		error_handling("listen() error");
 }
-void accept_connection(int serv_sock, struct sockaddr_in clnt_addr,int clnt_sock,socklen_t clnt_addr_size){
-	pthread_t t_id;
-	while(1){
-		clnt_addr_size = sizeof(clnt_addr);
-		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, (socklen_t*)&clnt_addr_size);
-		printf("Connection Request : %s:%d\n",inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
-		pthread_create(&t_id, NULL, request_handler, &clnt_sock);
-		pthread_detach(t_id);
-	}
-	close(serv_sock);
+void accept_connection(struct sockaddr_in clnt_addr,int clnt_sock,socklen_t clnt_addr_size){
+
+	 while (1) {
+        int *serv_sock = malloc(sizeof(int));
+        *serv_sock = accept(clnt_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+        if (*serv_sock < 0) {
+            perror("[ERR] failed to accept.\n");
+            free(serv_sock);
+            continue;
+        }
+
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, client_handler, (void *)serv_sock) != 0) {
+            perror("[ERR] failed to create thread.\n");
+            free(serv_sock);
+            close(*serv_sock);
+        }
+    }
+
+    close(clnt_sock);
 }
-void *request_handler(void *arg){
-	int clnt_sock = *((int*)arg);  
-	char req_line[SMALL_BUFF];
-	FILE* clnt_read;
-	FILE* clnt_write;
-	char method[SERV_SIZE];
-	char ct[SERV_SIZE];
-	char file_name[SERV_SIZE];
-
-	clnt_read = fdopen(clnt_sock, "r"); 
-	clnt_write = fdopen(dup(clnt_sock), "w");
-	fgets(req_line, SMALL_BUFF, clnt_read); 
-	
-	if(strstr(req_line, "HTTP/")==NULL){
-		send_error(clnt_write); 
-		fclose(clnt_read);
-		fclose(clnt_write);
-		return 0;
-	}
-	strcpy(method, strtok(req_line, " /"));
-	if(strcmp(method, "GET")!=0){
-		send_error(clnt_write); 
-		fclose(clnt_read); 
-		fclose(clnt_write);  
-		return 0;
-	}
-	strcpy(file_name, strtok(NULL, " /"));  
-	strcpy(ct, content_type(file_name));  
-	fclose(clnt_read);  
-	send_data(clnt_write, ct, file_name); 
-
-	return 0;
+void fill_header(char *header, int status, long len, char *type) {
+    char status_text[40];
+     switch (status) {
+        case 200:
+            strcpy(status_text, "OK");
+            break;
+        case 404:
+            strcpy(status_text, "Not Found");
+            break;
+        case 500:
+        default:
+            strcpy(status_text, "Internal Server Error");
+            break;
+    }
+    sprintf(header, HEADER_FMT, status, status_text, len, type);
 }
-void send_data(FILE * fp, char* ct, char*file_name){
-	char protocol[] = "HTTP/1.0 200 OK\r\n"; 
-	char server[] = "Server:Linux Web Server\r\n";
-	char cnt_len[] = "Content-lenght:2048\r\n";
-	char cnt_type[SMALL_BUFF]; 
-	char buf[BUFF_SIZE];
-	FILE* send_file;  
 
-	sprintf(cnt_type, "Content-type :%s\r\n\r\n", ct);  
-	send_file = fopen(file_name, "r"); 
-	if(send_file == NULL){
-		send_error(fp); 
-		fclose(fp);
-		return;
-	}
-	fputs(protocol, fp);
-	fputs(server, fp);
-	fputs(cnt_len,fp);
-	fputs(cnt_type, fp);
-
-	while(fgets(buf, BUFF_SIZE, send_file) != NULL){
-		fputs(buf,fp);
-		fflush(fp);
-	}
-	fflush(fp); 
-	fclose(fp); 
+void content_type(char *ct_type, char *uri) {
+    char *ext = strrchr(uri, '.');
+    if (!strcmp(ext, ".html"))
+        strcpy(ct_type, "text/html");
+    else if (!strcmp(ext, ".jpg") || !strcmp(ext, ".jpeg"))
+        strcpy(ct_type, "image/jpeg");
+    else if (!strcmp(ext, ".png"))
+        strcpy(ct_type, "image/png");
+    else if (!strcmp(ext, ".css"))
+        strcpy(ct_type, "text/css");
+    else if (!strcmp(ext, ".js"))
+        strcpy(ct_type, "text/javascript");
+    else
+        strcpy(ct_type, "text/plain");
 }
-char *content_type(char* file){
-	char extension[SMALL_BUFF];
-	char file_name[SMALL_BUFF];
 
-	strcpy(file_name, file);
-	strtok(file_name, ".");
-	strcpy(extension, strtok(NULL, "."));
+void handle_404(int asock) {
+    char header[BUF_SIZE];
+    fill_header(header, 404, sizeof(NOT_FOUND_CONTENT), "text/html");
 
-	return "text/plain";
+    write(asock, header, strlen(header));
+    write(asock, NOT_FOUND_CONTENT, sizeof(NOT_FOUND_CONTENT));
 }
-void send_error(FILE *fp){
-	char protocol[]="HTTP/1.0 400 Bad Request\r\n";
-	char server[]="Server:Linux Web Server\r\n";
-	char cnt_len[]="Content-length:2048\r\n";
-	char cnt_type[]="Content-type:text/html\r\n\r\n";
-	char content[]= "<html><head><title>NETWORK</title><head>""<body><font size+=10><br>오류 발생 요청 파일명 및 요청 방식 확인""</font></body></html>";
 
-	fputs(protocol,fp);
-	fputs(server,fp);
-	fputs(cnt_len,fp);
-	fputs(cnt_type,fp);
-	fflush(fp);
+void handle_500(int asock) {
+    char header[BUF_SIZE];
+    fill_header(header, 500, sizeof(SERVER_ERROR_CONTENT), "text/html");
+
+    write(asock, header, strlen(header));
+    write(asock, SERVER_ERROR_CONTENT, sizeof(SERVER_ERROR_CONTENT));
 }
+
+void *client_handler(void *arg) {
+    int client_sock = *((int *)arg);
+    free(arg);
+
+    char header[BUF_SIZE];
+    char buf[BUF_SIZE];
+
+    if (read(client_sock, buf, BUF_SIZE) < 0) {
+        perror("[ERR] Failed to read request.\n");
+        handle_500(client_sock);
+        close(client_sock);
+        pthread_exit(NULL);
+    }
+    char *method = strtok(buf, " ");
+    char *uri = strtok(NULL, " ");
+    fprintf(stderr, "Connection open\n");
+    
+    if (method == NULL || uri == NULL) {
+        perror("[ERR] Failed to identify method, URI.\n");
+        handle_500(client_sock);
+        close(client_sock);
+        pthread_exit(NULL);
+        fprintf(stderr, "Connection closed\n");
+    }
+    char safe_uri[BUF_SIZE];
+    char *local_uri;
+    struct stat st;
+
+    strcpy(safe_uri, uri);
+   
+     local_uri = safe_uri + 1;
+     if (stat(local_uri, &st) < 0) {
+        handle_404(client_sock);
+        close(client_sock);
+        pthread_exit(NULL);
+        fprintf(stderr, "Connection closed\n");
+    }  
+    int fd = open(local_uri, O_RDONLY);
+    if (fd < 0) {
+        handle_500(client_sock);
+        close(client_sock);
+        pthread_exit(NULL);
+        fprintf(stderr, "Connection closed\n");
+    }
+
+    int ct_len = st.st_size;
+    char ct_type[40];
+    content_type(ct_type, local_uri);
+    fill_header(header, 200, ct_len, ct_type);
+    write(client_sock, header, strlen(header));
+
+    int cnt;
+    while ((cnt = read(fd, buf, BUF_SIZE)) > 0)
+        write(client_sock, buf, cnt);
+        
+    fprintf(stderr, "Connection closed\n");
+    close(client_sock);
+    pthread_exit(NULL);
+}
+
 void error_handling(char *message){
 	fputs(message,stderr);
 	fputc('\n', stderr);
-	return ;
+	return;
 }
